c3c5be533d04a8b98c4fea5e4e43e50c223e53d2
HassBud <jennifer@bude.se>
2023-07-01 11:38:44 +0200

    Fixat InventorySystemTest

--- Test/se/kth/iv1350/sempos/model/CashRegisterTest.java
+++ Test/se/kth/iv1350/sempos/model/CashRegisterTest.java
@@ -9,30 +9,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashRegisterTest {
     CashRegister testChange;
     PaymentDTO testPayment;
-
     @BeforeEach
     void setUp() {
         testChange = new CashRegister();
         testPayment = new PaymentDTO(200,50,150);
-
     }
-
     @AfterEach
     void tearDown() {
         testChange = null;
-
+        testPayment = null;
     }
-
     @Test
     void pay() {
-        double changeToCostumer = testChange.getPaymentInfo().getChange();
+        double changeToCostumer = testPayment.getChange();
         testChange.pay(200,50);
         double countedChange = testChange.getPaymentInfo().getChange();
         assertNotEquals(changeToCostumer,countedChange, "Failed to match change");
 
     }
 
-    @Test
-    void getPaymentInfo() {
-    }
+  //  @Test
+  //  void getPaymentInfo() {
+  //  }
 }
