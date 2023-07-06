345b0d6b871f9fe43dd9f82968dbef7d249f33b8
MickeR94 <mikael.rudenvald@hotmail.se>
2023-07-06 11:24:50 +0200

    javadoc i CashRegister

--- src/se/kth/iv1350/sempos/model/CashRegister.java
+++ src/se/kth/iv1350/sempos/model/CashRegister.java
@@ -1,20 +1,28 @@
 package se.kth.iv1350.sempos.model;
 
+/**Represents the cash register in the store.
+ *
+ */
 public class CashRegister {
 
     private PaymentDTO paymentInfo;
 
-
-    /*The pay method has the variables paymentByCustomer and totalPayment as parameters.
-    * It calculates the difference between what the customer gave as payment and what the item(s) actually cost.
-    * The customer gets the difference back as change. */
+    /**
+     * The <code>PaymentDTO</code> pay method calculates change the customer will recieve
+     * @param paymentByCostumer How much money the customer should pay
+     * @param totalPayment How much money the customer gives
+     * @return The complete transaction
+     */
     public PaymentDTO pay(double paymentByCostumer, double totalPayment){
         double change =  totalPayment - paymentByCostumer;
         paymentInfo = new PaymentDTO(totalPayment,paymentByCostumer,change);
         return paymentInfo;
     }
 
-    /* */
+    /**
+     * The <code>PaymentDTO</code> getPaymentInfo getter
+     * return The complete transaction
+     */
     public PaymentDTO getPaymentInfo() {
 
         return paymentInfo;
