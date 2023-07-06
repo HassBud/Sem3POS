e7cd730fc6392fcfe1e385dc799971d7332ca199
HassBud <jennifer@bude.se>
2023-07-06 18:13:27 +0200

    kommentar på AccountingSystem.

--- src/se/kth/iv1350/sempos/model/Item.java
+++ src/se/kth/iv1350/sempos/model/Item.java
@@ -4,34 +4,42 @@ package se.kth.iv1350.sempos.model;
 public class Item{
 
 
+
     private int numberOfItems;
     ItemDTO itemDTO;
 
 
-    /*Item constructor. Starts out with 0 items stored */
+    /**
+     * Item constructor creates new instances representing an Item.
+     * @param newItemDTO represents an item with all the information regarding the item.
+     *
+     * */
     public Item(ItemDTO newItemDTO){
         itemDTO = newItemDTO;
         numberOfItems = 0;
     }
 
-    //should have a getIdentifierOfItem/setidentifierOfItem
-    //incrementNumberOfItems --> see the new class Item
-    public ItemDTO getItemDTO() {
-        return this.itemDTO;
-    }
-
-
-
-
-    //method for increment number of items
+    /**
+     * increases number of items with added quantity
+     * @param quantity number of items added.
+     */
 
     public void incrementNumberOfItems(int quantity) {
         numberOfItems = numberOfItems + quantity;
     }
-    //getter for numberOfItems
+
+    /**
+     * Getter for the attribute.
+     */
+
+    public ItemDTO getItemDTO() {
+        return this.itemDTO;
+    }
+
     public int getNumberOfItems() {
         return numberOfItems;
     }
+
 
 
 }
