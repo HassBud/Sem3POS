e1d3c9fce4aed38836339f8bb9c562ea040f1dbd
HassBud <jennifer@bude.se>
2023-08-05 11:14:33 +0200

    kommentar på AccountingSystem.

---
+++ src/se/kth/iv1350/sempos/Integration/DatabaseFailureException.java
@@ -0,0 +1,8 @@
+package se.kth.iv1350.sempos.Integration;
+
+public class DatabaseFailureException extends Exception {
+    public DatabaseFailureException(String exceptionMessage){
+        super(exceptionMessage);
+    }
+
+}
