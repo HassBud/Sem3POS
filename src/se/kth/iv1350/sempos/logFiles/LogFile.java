c78f3243f376d7214156f2d03a573c4453507c98
HassBud <jennifer@bude.se>
2023-08-13 18:03:41 +0200

    Tillagt package logFiles. Klasser LogFile och Log.

--- src/se/kth/iv1350/sempos/logFiles/LogFile.java
+++ src/se/kth/iv1350/sempos/logFiles/LogFile.java
@@ -7,7 +7,7 @@ public class LogFile {
 
     public LogFile(){
         try {
-            printStream = new PrintWriter(new FileWriter("printedLog.txt"), true);
+            printStream = new PrintWriter(new FileWriter("printed_file.txt"), true);
         } catch (IOException e) {
             System.out.println("Not possible to Log.");
             e.printStackTrace();
