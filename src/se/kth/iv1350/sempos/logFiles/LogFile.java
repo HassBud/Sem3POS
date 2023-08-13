b1ee284964fe158496d2b6d10963dca09fe38efc
HassBud <jennifer@bude.se>
2023-08-13 18:59:46 +0200

    Update project.

--- src/se/kth/iv1350/sempos/logFiles/LogFile.java
+++ src/se/kth/iv1350/sempos/logFiles/LogFile.java
@@ -17,4 +17,13 @@ public class LogFile {
     public void logMessage(String message){
         printStream.println(message);
     }
+
+    public void loggedException(Exception exception){
+        StringBuilder messageThatGetsLogged = new StringBuilder();
+        messageThatGetsLogged.append(", The thrown Exception: ");
+        messageThatGetsLogged.append(exception.getMessage());
+        printStream.println(messageThatGetsLogged);
+        exception.printStackTrace(printStream);
+        printStream.println("\n");
+    }
 }
