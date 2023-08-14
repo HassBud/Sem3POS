ad59b4a6754267d98ec9e4a47c3413d8870a5f15
HassBud <jennifer@bude.se>
2023-08-14 22:08:22 +0200

    Update project.

--- src/se/kth/iv1350/sempos/view/TotalRevenueView.java
+++ src/se/kth/iv1350/sempos/view/TotalRevenueView.java
@@ -2,12 +2,11 @@ package se.kth.iv1350.sempos.view;
 
 import se.kth.iv1350.sempos.model.Observer;
 import se.kth.iv1350.sempos.model.PaymentDTO;
-import se.kth.iv1350.sempos.model.Sale;
 
 public class TotalRevenueView implements Observer {
+
     @Override
-    public void Sale(PaymentDTO saleInfo) {
-        PaymentDTO totalRevenueForEachSale = saleInfo;
+    public void displayRevenue(PaymentDTO totalRevenue) {
 
     }
 }
