67eb5f7b51214c0f541922e9984c154ba2c0b72d
HassBud <jennifer@bude.se>
2023-08-21 09:46:28 +0200

    Update project.

--- Test/se/kth/iv1350/sempos/model/InvalidIdentifierOfItemExceptionTest.java
+++ Test/se/kth/iv1350/sempos/model/InvalidIdentifierOfItemExceptionTest.java
@@ -9,31 +9,16 @@ import se.kth.iv1350.sempos.Integration.InventorySystem;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InvalidIdentifierOfItemExceptionTest {
-    InvalidIdentifierOfItemException testException;
-    InventorySystem testItemID;
 
-    @BeforeEach
-    void setUp() {
-        String exceptionMessage = "Testing exception for invalid ID";
-        testException = new InvalidIdentifierOfItemException(exceptionMessage);
-        testItemID = new InventorySystem();
-    }
+    InventorySystem inventory;
+@Test
+    public void InvalidIdentifier(){
+    String exceptionMessage = "Item ID not valid";
+    InvalidIdentifierOfItemException noValidID = new InvalidIdentifierOfItemException(exceptionMessage);
+    assertEquals(exceptionMessage,noValidID.getMessage(), "The Item ID does not exist. ");
 
-    @AfterEach
-    void tearDown() {
-        testException = null;
-        testItemID = null;
-    }
 
-    @Test
-    void getItemInformation() throws InvalidIdentifierOfItemException, DatabaseFailureException {
-        ItemDTO firstItem = testItemID.getItemInformation(1);
-        assertEquals(1,firstItem.getIdentifierOfItem(), "Item ID does not match");
-        ItemDTO secondItem = testItemID.getItemInformation(2);
-        assertEquals(2,secondItem.getIdentifierOfItem(), "Item ID does not match");
-        ItemDTO thirdItem = testItemID.getItemInformation(3);
-        assertEquals(3,thirdItem.getIdentifierOfItem(), "Item ID does not match");
-        String exceptionMessage = "Testing exception for invalid ID";
-        assertEquals(exceptionMessage, testException.getMessage(), "Item ID is not existing.");
-    }
+}
+
+
 }
